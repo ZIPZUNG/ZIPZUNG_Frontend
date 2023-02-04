b87ff9b6f3dedd8f5d51f764477286395cf19986
Ubinquitous <ubinquitous1@gmail.com>
2023-02-04 11:13:40 +0900

    ADD : Detail, Home, Search page publish

--- src/allFiles.ts
+++ src/allFiles.ts
@@ -1,3 +1,6 @@
 import Home from './pages/Home'
+import Header from 'components/Header'
+import Search from 'pages/Search'
+import Detail from 'pages/Detail'
 
-export { Home }
+export { Home, Header, Search, Detail }
